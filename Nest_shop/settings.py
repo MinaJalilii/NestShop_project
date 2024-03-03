ac0b04285eb88f4454c3bf9a3e004a784dc4bb53
Mina <hominaa99@gmail.com>
2024-03-03 10:50:15 +0330

    vercel confiuration updated

--- Nest_shop/settings.py
+++ Nest_shop/settings.py
@@ -151,7 +151,7 @@ MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 AUTH_USER_MODEL = 'userauths.User'
 
